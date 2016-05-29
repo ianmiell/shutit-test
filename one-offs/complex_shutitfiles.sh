152f68c5943eeb339891add61757ac11314cc04e
Ian Miell <ian.miell@gmail.com>
2016-05-29 18:15:40 +0100

    Delivery

---
+++ one-offs/complex_shutitfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+rm -rf /tmp/asd
+pushd ..
+shutit skeleton \
+	--shutitfiles test/shutitfile_complex/ShutItFile \
+	test/shutitfile_complex/ShutItFile2 \
+	--module_directory /tmp/asd \
+	--module_name testing \
+	--domain shutit.tk \
+	--delivery bash \
+	--template_branch bash   
+popd
