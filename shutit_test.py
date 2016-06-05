069f44f8bc3f2179162288335eb8125163f8bcc5
Ian Miell <ian.miell@gmail.com>
2016-06-05 12:11:20 +0100

    COPY and ADD

--- shutit_test.py
+++ shutit_test.py
@@ -21,6 +21,7 @@ class shutit_test(ShutItModule):
 		shutit.send('cd ..')
 		shutit.send('git clone https://github.com/ianmiell/shutit-test')
 		shutit.send('cd shutit-test/test')
+		# TODO: divide up tests and make optional
 		shutit.send('./test.sh')
 
 		shutit.logout()
