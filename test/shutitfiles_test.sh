e3e3d87522e0771084e1e1a28f10f39071857eba
Ian Miell <ian.miell@gmail.com>
2016-06-20 12:02:18 +0100

    sht

---
+++ test/shutitfiles_test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Copyright (C) 2014 OpenBet Limited
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+TESTS=${1:-basic}
+
+pushd $(dirname ${BASH_SOURCE[0]})/.. > /dev/null 2>&1
+
+#set -x
+
+source test/shared_test_utils.sh
+
+# Variables
+NEWDIR=/tmp/shutit_testing_$$_$(hostname)_$(whoami)_$(date -I)_$(date +%N)
+SHUTIT_DIR="$(pwd)"
+readonly NEWDIR SHUTIT_DIR
+
+#set_shutit_options
+
+# Check we can use docker
+if ! $DOCKER info >/dev/null 2>&1; then
+	echo "Failed to run docker! - used command \"$DOCKER info\" to check"
+	false
+fi
+
+# This is a fallback, any tests runnable on their own should include the below
+if [[ $0 != shutitfiles_test.sh ]] && [[ $0 != ./shutitfiles_test.sh ]]
+then
+	echo "Must be run from dir of shutitfile_test.sh"
+	exit 1
+fi
+
+pushd test/shutitfiles
+for d in $(ls -d *)
+do
+	pushd $d
+	./test.sh -l debug
+	if [[ $? != 0 ]]
+	then
+		popd
+		exit 1
+	fi
+	popd
+done
+popd
+
+report
+cleanup hard
+
+popd > /dev/null 2>&1
+# OK
+echo "SHUTIT TEST OK"
+exit
+
