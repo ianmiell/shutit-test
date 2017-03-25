c01c5ea6ff1f2485988a1feb02b525ef2d451105
Ian Miell <ian.miell@gmail.com>
2017-03-25 08:33:32 +0000

    23

---
+++ test/docker_tests/23/Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:16.04
+
+RUN apt-get update
+RUN apt-get install -y -qq git python-pip python-dev
+RUN pip install shutit
+
+WORKDIR /opt
+# Change the next two lines to build your ShutIt module.
+RUN git clone https://github.com/yourname/yourshutitproject.git
+WORKDIR /opt/yourshutitproject
+RUN shutit build --delivery dockerfile
+CMD ["/bin/bash"]
---
+++ test/docker_tests/23/test23.py
@@ -0,0 +1,17 @@
+from shutit_module import ShutItModule
+
+class test23(ShutItModule):
+
+
+	def build(self, shutit):
+		shutit.send_host_dir('/configs','configs')
+		return True
+
+def module():
+	return test23(
+		'docker_tests.test23.test23', 319863921.0001,
+		description='',
+		maintainer='',
+		delivery_methods=['docker'],
+		depends=['shutit.tk.setup']
+	)
