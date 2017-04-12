d09ab024c92664c051c569c12e00a1c178d73fd7
Ian Miell <ian.miell@gmail.com>
2017-04-12 11:11:41 +0100

    send_host_dir

--- test/docker_tests/23/test23.py
+++ test/docker_tests/23/test23.py
@@ -1,14 +1,15 @@
 from shutit_module import ShutItModule
+import os
 
 class test23(ShutItModule):
 
-
 	def build(self, shutit):
-		shutit.send_host_dir('/configs','configs')
+		shutit.send_host_dir('/','configs')
 		shutit.send('ls -R /configs')
 		if shutit.send_and_get_output('ls /configs | grep build.cnf | wc -l') != '1':
 			shutit.fail('wrong number of files')
-		shutit.send_host_dir('/contents','../..')
+		shutit.send_host_dir('/contents',os.path.abspath(os.curdir + '/../..'))
+		shutit.send('ls -R /contents')
 		return True
 
 def module():
