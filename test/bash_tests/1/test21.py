a11d87440140e001712b46da7792535f364970e0
Ian Miell <ian.miell@gmail.com>
2017-04-22 09:00:31 +0100

    comment

--- test/bash_tests/1/test21.py
+++ test/bash_tests/1/test21.py
@@ -6,7 +6,7 @@ class test21(ShutItModule):
 	def build(self, shutit):
 		shutit.send('ping -c 1 -t 1 shutit.tk',follow_on_commands={'.*0 packets received.*':'echo error > /tmp/shutittk_pingres','.*1 packets received.*':'echo ok > /tmp/shutittk_pingres','.*Unknown host.*':'echo unknown > /tmp/shutittk_pingres'},check_exit=False)
 		shutit.send('ping -c 1 -t 1 asdasd',follow_on_commands={'.*0 packets received.*':'echo error > /tmp/bogus_pingres','.*1 packets received.*':'echo ok > /tmp/bogus_pingres','.*Unknown host.*':'echo unknown > /tmp/bogus_pingres'},check_exit=False)
-		shutit.send('ping -c 1 -t 1 bbc.co.uk',follow_on_commands={'.*0 packets received.*':'echo error > /tmp/bbc_pingres','.*1 packets received.*':'echo ok > /tmp/bbc_pingres','.*Unknown host.*':'echo unknown > /tmp/bbc_pingres'},check_exit=False)
+		#shutit.send('ping -c 1 -t 1 bbc.co.uk',follow_on_commands={'.*0 packets received.*':'echo error > /tmp/bbc_pingres','.*1 packets received.*':'echo ok > /tmp/bbc_pingres','.*Unknown host.*':'echo unknown > /tmp/bbc_pingres'},check_exit=False)
 		output=shutit.send_and_get_output('cat /tmp/*pingres')
 		assert output == '''ok
 unknown
