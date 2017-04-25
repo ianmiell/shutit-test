69249b0e8720d75b5a324fd97697ca96b98b4122
Ian Miell <ian.miell@gmail.com>
2017-04-25 20:06:40 +0100

    multitest

---
+++ test/bash_tests/3/test3.py
@@ -0,0 +1,20 @@
+from shutit_module import ShutItModule
+
+class test3(ShutItModule):
+
+	def build(self, shutit):
+		s1=shutit.create_session()
+		s1out=s1.send_and_get_output('echo $$')
+		s2=shutit.create_session()
+		s2out=s2.send_and_get_output('echo $$')
+		assert s1out != s2out
+		return True
+
+def module():
+		return test3(
+			'bash_tests.test3.test3', 2010087968.000112535246,
+			description='',
+			maintainer='',
+			delivery_methods=['bash'],
+			depends=['shutit.tk.setup']
+		)
