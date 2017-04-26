6d0d05d3f7461dcb00244d31fdb6320d98bb75a5
Ian Miell <ian.miell@gmail.com>
2017-04-26 12:14:38 +0100

    number

--- test/bash_tests/2/test2.py
+++ test/bash_tests/2/test2.py
@@ -23,7 +23,7 @@ class test2(ShutItModule):
 
 def module():
 		return test2(
-			'bash_tests.test2.test2', 2010087968.000112535246,
+			'bash_tests.test2.test2', 2010087968.0125012500112535246,
 			description='',
 			maintainer='',
 			delivery_methods=['bash'],
