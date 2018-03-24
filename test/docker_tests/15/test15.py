ae389c04d29c8dec2d3e2099594f8cebab223410
Ian Miell <ian.miell@gmail.com>
2018-03-24 09:41:57 +0000

    atest

--- test/docker_tests/15/test15.py
+++ test/docker_tests/15/test15.py
@@ -5,6 +5,7 @@ class test15(ShutItModule):
 	def build(self, shutit):
 		if not shutit.send_and_require('echo up simba',['.*p sim.*']):
 			return False # pragma: no cover
+		# This should result in a pause point if in interactive mode...
 		if shutit.send_and_require('echo up bimba',['.*p sim.*']):
 			return False # pragma: no cover
 		shutit.send('sleep 15 && touch /tmp/asdfghjkl &')
